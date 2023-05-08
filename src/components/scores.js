a4d044ced33aad519b70cbe708973c86423bd92c
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-05-08 13:32:46 -0700

    Setup submission of score

--- src/components/scores.js
+++ src/components/scores.js
@@ -1,6 +1,8 @@
 import { useLocation } from "react-router-dom";
+import {useState} from 'react'
 
 function Scores() {
+    const [name, setName] = useState("")
     const { state } = useLocation();
     const time = state.time;
 
@@ -8,14 +10,24 @@ function Scores() {
     function str_pad_left(string, pad, length) {
         return (new Array(length + 1).join(pad) + string).slice(-length);
       }
-      const minutes = Math.floor(time / 60);
-      const seconds = time - minutes * 60;
+    const minutes = Math.floor(time / 60);
+    const seconds = time - minutes * 60;
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name)
+    }
 
     return (
         <div className="scores">
-            <form>
-                <input type="text" placeholder="Name"></input>
+            <form className="enter-score" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Name" value={name} onChange={handleNameChange}></input>
                 <p className="last-score">{ str_pad_left(minutes, '0', 2) + ':' + str_pad_left(seconds , '0', 2) }</p>
+                <input type='submit'></input>
             </form>
         </div>
     )
